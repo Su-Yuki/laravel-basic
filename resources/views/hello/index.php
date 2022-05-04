87411c005c715c2283f014e61bdd3bc418ee8bb0
Su-Yuki <76637981+Su-Yuki@users.noreply.github.com>
2022-05-04 20:18:26 +0900

    dami

---
+++ resources/views/hello/index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Hello/Index</title>
+    <style>
+      body {font-size: 16px; color: #999;}
+      h1   {font-size: 100pt; text-align: right; color:#eee; margin:40 0 -50 0;}
+    </style>
+  </head>
+  <body>
+      <h1>Index</h1>
+      <p>This is a sample page with php-templete</p>
+  </body>
+</html>
